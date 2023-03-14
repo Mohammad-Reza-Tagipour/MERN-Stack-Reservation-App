9eee391306c24b71c6570dccb0aea2281f6a3601
Mohammadreza <mohammadrezatagipour45@gmail.com>
2023-03-14 03:59:34 +0330

    react starting files added

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -19,6 +19,16 @@ router.post("/", async (req, res) => {
 });
 
 // UPDATE
+router.put("/", async (req, res) => {
+  try {
+    const updateHotel = await newHotel.save();
+    res.status(200).json(savedHotel);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// DELETE
 
 // GET
 
